687e8a6a2a84aecde19967a3e56b45f22b6deda9
최연지 <rokmc3038@naver.com>
2023-12-13 20:08:04 +0900

    서비스 경로 변경

--- src/main/java/com/example/demo/main/MainService.java
+++ src/main/java/com/example/demo/main/MainService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
 public class MainService {
-	@RequestMapping("mainform")
+	@RequestMapping("main/mainform")
 	public void mainform(String cp, Model model) {
 		
 	}
