de692d3bb6a415445dd0265e649716b4bfd3bd4d
최연지 <rokmc3038@naver.com>
2024-01-03 20:09:42 +0900

    mapper 생성

--- src/main/java/com/example/demo/notice/NoticeController.java
+++ src/main/java/com/example/demo/notice/NoticeController.java
@@ -3,12 +3,13 @@ package com.example.demo.notice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class NoticeController {
@@ -24,27 +25,79 @@ public class NoticeController {
         return "/notice/noticeform";
     }
 
-    // @RequestMapping("noticeContent")
-    // public String noticeContent(String no, Model model) {
-    // NoticeDTO notice = service.noticeContent(no);
-    // if (notice == null) {
-    // return "redirect:noticeForm";
-    // }
+    @RequestMapping("/notice/noticewrite")
+    public String noticewrite() {
+        String sessionId = (String) session.getAttribute("id");
+        if (sessionId == null)
+            // return "redirect:login";
+            sessionId = "admin";
+        return "/notice/noticewrite";
+    }
 
-    // model.addAttribute("notice", notice);
-    // return "notice/noticeContent";
-    // }
+    @PostMapping("/notice/noticewriteProc")
+    public String noticewriteProc(MultipartHttpServletRequest multi) {
+        String sessionId = (String) session.getAttribute("id");
+        if (sessionId == null)
+            sessionId = "admin";
+        // return "redirect:login";
+        String path = service.noticewriteProc(multi);
+        return path;
+    }
+
+    @RequestMapping("/notice/noticecontent")
+    public String noticecontent(String no, Model model) {
+        NoticeDTO Notice = service.noticecontent(no);
+        if (Notice == null) {
+            return "redirect:noticeform";
+        }
+
+        model.addAttribute("Notice", Notice);
+        return "/notice/noticecontent";
+    }
 
     @RequestMapping("noticeDownload")
     public void noticeDownload(String no, HttpServletResponse response) {
         service.noticeDownload(no, response);
     }
 
-    @RequestMapping("notice/noticewrite")
-    public String noticewrite(Model model, @RequestParam(value = "currentPage", required = false) String cp) {
-        service.noticewrite(cp, model);
+    // @RequestMapping("/notice/noticemodify")
+    // public String noticemodify(String no, Model model) {
+    // String sessionId = (String) session.getAttribute("id");
+    // if (sessionId == null)
+    // sessionId = "admin";
+    // // return "redirect:login";
+    // String path = service.noticemodify(no, model);
+    // return path;
+    // }
 
-        return "notice/noticewrite";
-    }
+    // @PostMapping("noticemodifyProc")
+    // public String noticemodifyProc(NoticeDTO notice, RedirectAttributes ra) {
+    // String sessionId = (String) session.getAttribute("id");
+    // if (sessionId == null)
+    // sessionId = "admin";
+    // // return "redirect:login";
+    // String msg = service.noticemodifyProc(Notice);
+    // ra.addFlashAttribute("msg", msg);
+
+    // if (msg.equals("게시글 수정 성공"))
+    // return "redirect:noticecontent?no=" + Notice.getNo();
+
+    // return "redirect:noticemodify?no=" + Notice.getNo();
+    // }
+
+    // @RequestMapping("noticeDeleteProc")
+    // public String noticeDeleteProc(String no) {
+    // String sessionId = (String) session.getAttribute("id");
+    // if (sessionId == null)
+    // sessionId = "admin";
+    // // return "redirect:login";
+
+    // String msg = service.noticeDeleteProc(no);
+
+    // if (msg.equals("작성자만 삭제 할 수 있습니다."))
+    // return "redirect:NoticeContent?no=" + no;
+
+    // return "redirect:NoticeForm";
+    // }
 
 }
--- src/main/java/com/example/demo/notice/NoticeService.java
+++ src/main/java/com/example/demo/notice/NoticeService.java
@@ -2,6 +2,7 @@ package com.example.demo.notice;
 
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -18,6 +19,14 @@ public class NoticeService {
     }
 
     public void noticewrite(String cp, Model model) {
+    }
+
+    public String noticewriteProc(MultipartHttpServletRequest multi) {
+        return null;
+    }
+
+    public NoticeDTO noticecontent(String no) {
+        return null;
     }
 
 }
