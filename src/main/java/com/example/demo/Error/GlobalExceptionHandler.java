dc30a033074bdcf990e3e43bc9f4c95966d5ee94
xoxxxoox <demisoda7979@naver.com>
2024-01-31 13:41:01 +0900

    errorPage add

---
+++ src/main/java/com/example/demo/Error/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.example.demo.Error;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handle500Error(Exception e) {
+        return "error500";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handle404Error(NoHandlerFoundException e) {
+        return "error404";
+    }
+    
+}
